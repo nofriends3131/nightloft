0f12f92cfdff1f3dbbadc586799307a34dc97c92
nofriends3131 <trsatarashiiyt.31@gmail.com>
2025-03-26 18:31:17 +0800

    werhfgbrtfgburt

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,21 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tshirt1" element={<Tshirt1Page />} />
-        <Route path="/hoodie" element={<HoodiePage />} />
-        <Route path="/tshirt2" element={<Tshirt2Page />} />
-        <Route path="/compression" element={<CompressionPage />} />
-      </Routes>
-    </Router>
+    <>
+      <div className="stars-small"></div>
+      <div className="stars-medium"></div>
+      <div className="nebula"></div>
+      <div className="moon"></div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tshirt1" element={<Tshirt1Page />} />
+          <Route path="/hoodie" element={<HoodiePage />} />
+          <Route path="/tshirt2" element={<Tshirt2Page />} />
+          <Route path="/compression" element={<CompressionPage />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
